56253af06c098696f674e5b9e964d712f72056af
JaeseongYoo <praster1@gmail.com>
2018-12-12 09:01:26 +0900

    Update basic_analysis.R

--- basic_analysis.R
+++ basic_analysis.R
@@ -31,63 +31,83 @@ par(mfrow = c(6, 1))
 
 source("synthetic_pureRP.R")
 dataVec = synthetic_pureRP(constMean = 0, mean = 0, sd = 1, length = dataLen)
-ts.plot(dataVec, main = "Example of (random) stationary time series", ylab = expression(X[t]))
+plot(dataVec, main = "Example of (random) stationary time series", ylab = expression(X[t]), type="l")
+
+
+
+# outlier
+   # - Additive Outliers (AO): 시계열 자료 중 한 개가 지나치게 크거나 작은 값을 갖는 관측치
+   # - innovational outliers(IO): 이후의 시계열이 전혀 다른 형태를 보이는 특징을 가지는 이상치
+   # - level shift outliers(LSO): 이후에 시계열 전체가 위나 아래로 이동한 경우
+   # - temporary chance outlers(TCO): 일시적으로 시계열이 이동하였으나 지수적으로 빠르게 원래의 상태로 돌아가는 시계열의 형태
+   # - seasonal level shift(SLS): 계절적으로 특정한 시기에 나타나는 이상치
+   # - variance change(VC): 이후에 시간이 흐를 수록 변동폭이 커지거나 작아지는 이상치
 
 outlierIndexs = sort(ceiling(runif(5, 1,dataLen)))
 
 outlierIndexs_IO = outlierIndexs[1]
 dataVec[outlierIndexs_IO:dataLen] = synthetic_pureRP(constMean = 0, mean = 0, sd = runif(1, 0, 3), length = length(outlierIndexs_IO:dataLen))
-ts.plot(dataVec, main = "Example of (random) stationary time series with IO", ylab = expression(X[t]))
+plot(dataVec, main = "Example of (random) stationary time series with IO", ylab = expression(X[t]), type="l")
+points(outlierIndexs_IO, dataVec[outlierIndexs_IO], col="red", lwd=5)
 
 outlierIndexs_LSO = outlierIndexs[2]
 dataVec[outlierIndexs_LSO:dataLen] = synthetic_pureRP(constMean = 0, mean = runif(1, -3, 3), sd = runif(1, 0, 3), length = length(outlierIndexs_LSO:dataLen))
-ts.plot(dataVec, main = "Example of (random) stationary time series with LSO", ylab = expression(X[t]))
+plot(dataVec, main = "Example of (random) stationary time series with LSO", ylab = expression(X[t]), type="l")
+points(outlierIndexs_LSO, dataVec[outlierIndexs_LSO], col="red", lwd=5)
 
 outlierIndexs_TCO = outlierIndexs[3:4]
 dataVec[outlierIndexs_TCO[1]:outlierIndexs_TCO[2]] = dataVec[outlierIndexs_TCO[1]:outlierIndexs_TCO[2]] + runif(1, -3, 3)
-ts.plot(dataVec, main = "Example of (random) stationary time series with TCO", ylab = expression(X[t]))
+plot(dataVec, main = "Example of (random) stationary time series with TCO", ylab = expression(X[t]), type="l")
+points(outlierIndexs_TCO, dataVec[outlierIndexs_TCO], col="red", lwd=5)
 
 outlierIndexs_VC = outlierIndexs[5]
 addVarianceVec = seq(1, 3, length=length(outlierIndexs_VC:dataLen))
 dataVec[outlierIndexs_VC:dataLen] = dataVec[outlierIndexs_VC:dataLen] * addVarianceVec
-ts.plot(dataVec, main = "Example of (random) stationary time series with VC", ylab = expression(X[t]))
+plot(dataVec, main = "Example of (random) stationary time series with VC", ylab = expression(X[t]), type="l")
+points(outlierIndexs_VC, dataVec[outlierIndexs_VC], col="red", lwd=5)
 
 outlierIndexs_AO = runif(10, 1, dataLen)
 dataVec[outlierIndexs_AO] = dataVec[outlierIndexs_AO] + 10 * runif(10, -5, 5)
-ts.plot(dataVec, main = "Example of (random) stationary time series with AO", ylab = expression(X[t]))
+plot(dataVec, main = "Example of (random) stationary time series with AO", ylab = expression(X[t]), type="l")
+points(outlierIndexs_AO, dataVec[outlierIndexs_AO], col="red", lwd=5)
+
+
 
 
 
 ##### Random Walk process simulation
-par(mfrow = c(6, 1))
+# par(mfrow = c(6, 1))
 
-source("synthetic_randomWalk.R")
-dataVec = synthetic_randomWalk(initVal = 1000, mean = 0, sd = 1, length = dataLen)
-ts.plot(dataVec, main = "Random walk process")
+# source("synthetic_randomWalk.R")
+# dataVec = synthetic_randomWalk(initVal = 1000, mean = 0, sd = 1, length = dataLen)
+# ts.plot(dataVec, main = "Random walk process")
 
 
-outlierIndexs = sort(ceiling(runif(5, 1,dataLen)))
+# outlierIndexs = sort(ceiling(runif(5, 1,dataLen)))
 
-outlierIndexs_IO = outlierIndexs[1]
-dataVec[outlierIndexs_IO:dataLen] = synthetic_randomWalk(initVal = dataVec[outlierIndexs_IO-1], mean = 0, sd = runif(1, 0, 3), length = length(outlierIndexs_IO:dataLen))
-ts.plot(dataVec, main = "Example of (random) stationary time series with IO", ylab = expression(X[t]))
+# outlierIndexs_IO = outlierIndexs[1]
+# dataVec[outlierIndexs_IO:dataLen] = synthetic_randomWalk(initVal = dataVec[outlierIndexs_IO-1], mean = 0, sd = runif(1, 0, 3), length = length(outlierIndexs_IO:dataLen))
+# ts.plot(dataVec, main = "Example of (random) stationary time series with IO", ylab = expression(X[t]))
 
-outlierIndexs_LSO = outlierIndexs[2]
-dataVec[outlierIndexs_LSO:dataLen] = synthetic_randomWalk(initVal = dataVec[outlierIndexs_LSO-1], mean = 0, sd = runif(1, 0, 3), length = length(outlierIndexs_LSO:dataLen))
-ts.plot(dataVec, main = "Example of (random) stationary time series with LSO", ylab = expression(X[t]))
+# outlierIndexs_LSO = outlierIndexs[2]
+# dataVec[outlierIndexs_LSO:dataLen] = synthetic_randomWalk(initVal = dataVec[outlierIndexs_LSO-1], mean = 0, sd = runif(1, 0, 3), length = length(outlierIndexs_LSO:dataLen))
+# ts.plot(dataVec, main = "Example of (random) stationary time series with LSO", ylab = expression(X[t]))
 
-outlierIndexs_TCO = outlierIndexs[3:4]
-dataVec[outlierIndexs_TCO[1]:outlierIndexs_TCO[2]] = dataVec[outlierIndexs_TCO[1]:outlierIndexs_TCO[2]] + runif(1, -3, 3)
-ts.plot(dataVec, main = "Example of (random) stationary time series with TCO", ylab = expression(X[t]))
+# outlierIndexs_TCO = outlierIndexs[3:4]
+# dataVec[outlierIndexs_TCO[1]:outlierIndexs_TCO[2]] = dataVec[outlierIndexs_TCO[1]:outlierIndexs_TCO[2]] + runif(1, -3, 3)
+# ts.plot(dataVec, main = "Example of (random) stationary time series with TCO", ylab = expression(X[t]))
 
-outlierIndexs_VC = outlierIndexs[5]
-addVarianceVec = seq(1, 3, length=length(outlierIndexs_VC:dataLen))
-dataVec[outlierIndexs_VC:dataLen] = dataVec[outlierIndexs_VC:dataLen] * addVarianceVec
-ts.plot(dataVec, main = "Example of (random) stationary time series with VC", ylab = expression(X[t]))
+# outlierIndexs_VC = outlierIndexs[5]
+# addVarianceVec = seq(1, 3, length=length(outlierIndexs_VC:dataLen))
+# dataVec[outlierIndexs_VC:dataLen] = dataVec[outlierIndexs_VC:dataLen] * addVarianceVec
+# ts.plot(dataVec, main = "Example of (random) stationary time series with VC", ylab = expression(X[t]))
 
-outlierIndexs_AO = runif(10, 1, dataLen)
-dataVec[outlierIndexs_AO] = dataVec[outlierIndexs_AO] + 100 * runif(10, -5, 5)
-ts.plot(dataVec, main = "Example of (random) stationary time series with AO", ylab = expression(X[t]))
+# outlierIndexs_AO = runif(10, 1, dataLen)
+# dataVec[outlierIndexs_AO] = dataVec[outlierIndexs_AO] + 100 * runif(10, -5, 5)
+# ts.plot(dataVec, main = "Example of (random) stationary time series with AO", ylab = expression(X[t]))
+
+
+
 
 
 ##### Moving Average of order q: MA(q)
@@ -96,16 +116,25 @@ ts.plot(dataVec, main = "Example of (random) stationary time series with AO", yl
 # ts.plot(dataVec, main = "Moving Average or order 1 process")
 
 
+
+
+
 ##### Auto-Regressive of order p: AR(p)
 # source("synthetic_AR1.R")
 # dataVec = synthetic_AR1(initVal = 1, coef=-0.3, mean = 0, sd = 1.5, length = dataLen)
 # ts.plot(dataVec)
 
 
+
+
+
 ##### Autoregressive moving average process: ARMA(p,q)
 # source("synthetic_ARMA11.R")
 # dataVec = synthetic_ARMA11(initVal = 1000, coefAR=-0.45, coefMA=-0.45, mean = 0, sd = 1, length = dataLen)
 # ts.plot(X, main = "ARMA(1,1) process")
+
+
+
 
 
 ##### 전력데이터
@@ -134,9 +163,15 @@ datetime = seqDatetime_byLength(startDate="2015-09-01", length=length(dataVec), 
                       # from= "2013-01-01", 
                       # to = today, auto.assign = FALSE)
 
-
-
-
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
+                      
 ##### Sample Vector 리스트 구하기
 source("getPartialData.R")  # dataVec을 stepSize만큼 건너뛰면서 partialLength씩 자른다.
 
@@ -185,12 +220,12 @@ analysisRes = lapply(sampleVec_UnitRoot$data, ur.df, lags=lag_UnitRoot, type='tr
 source("plotAll.R")
 source("plotTrendTest.R")
 source("plotUnitRootTest.R")
-par(mfrow = c(6, 1))
+par(mfrow = c(4, 1))
 plotAll(dataVec, datetime)
 
 
-plot(decompose(ts(dataVec, frequency = 96*365))$trend, main="trend")
-plot(decompose(ts(dataVec, frequency = 96*365))$seasonal, main="sensonal")
+# plot(decompose(ts(dataVec, frequency = 96*365))$trend, main="trend")
+# plot(decompose(ts(dataVec, frequency = 96*365))$seasonal, main="sensonal")
 
 
 plotAll(dataVec, datetime)
