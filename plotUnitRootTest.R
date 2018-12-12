27e3d09d8063f26d33eab9503ee7cc72ecf66a76
JaeseongYoo <praster1@gmail.com>
2018-12-12 09:54:28 +0900

    20181212

--- plotUnitRootTest.R
+++ plotUnitRootTest.R
@@ -1,11 +1,16 @@
-plotUnitRootTest = function(dataVec, analysisResult, critVal = 3)
+plotUnitRootTest = function(dataVec, analysisResult, critVal = 3, print=FALSE)
 {
     len = length(dataVec$data)
     for (i in 1:len)
     {
         testStat = analysisResult[[i]]@teststat[1]
         cval = analysisResult[[i]]@cval[1,critVal]
-        print(paste("i:", i, "/", len, "     ", testStat < cval))
+        
+        if (print)
+        {
+            print(paste("i:", i, "/", len, "     ", testStat < cval))
+        }
+        
         if (testStat < cval)
         {
             points(dataVec$index[[i]], dataVec$data[[i]], type="l", col="red", lwd=5);	
