312142081a70e8f32604d14910950e21b80032b0
JaeseongYoo <praster1@gmail.com>
2018-12-17 04:44:57 +0900

    Results_IDXGap Experiments

---
+++ basic_analysis_IDXGap_Analysis.R
@@ -0,0 +1,25 @@
+rm(list = ls())
+
+
+
+setwd("/home/lv999/Dropbox/Github/Unit-root-test-of-Non-stationary-time-series")
+# setwd("E:/Dropbox/Github/Unit-root-test-of-Non-stationary-time-series")
+
+
+
+
+
+
+
+dataLen = 96*365
+IDXGap_ADFTrend = NULL
+IDXGap_ADFDrift = NULL
+IDXGap_PPTrend = NULL
+IDXGap_PPDrift = NULL
+IDXGap_ERSDGTrend = NULL
+IDXGap_ERSDGConst = NULL
+IDXGap_ERSPTrend = NULL
+IDXGap_ERSPConst = NULL
+IDXGap_SPTau = NULL
+IDXGap_SPRho = NULL
+IDXGap_KPSSTau = NULL
