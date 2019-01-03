229b3e00fc16f69dbea21d6a771a0db1d3ec1f7d
JaeseongYoo <praster1@gmail.com>
2019-01-03 14:36:36 +0900

    20190103

--- Unit Root Test in R/trycatch.py
+++ Unit Root Test in R/trycatch.py
@@ -47,7 +47,7 @@ def trycatch_lag(lag):
 
 
 ### embed function: wrong embedding dimension / dimension < 1
-def trycatch_embed(dimension):
+def trycatch_embed1(dimension):
  	try:
 		if (dimension < 1):
 			raise ValueError
@@ -56,7 +56,7 @@ def trycatch_embed(dimension):
 		 
 
 ### embed function: wrong embedding dimension / dimension > n
-def trycatch_embed(dimension, n):
+def trycatch_embed2(dimension, n):
  	try:
 		if (dimension > n):
 			raise ValueError
